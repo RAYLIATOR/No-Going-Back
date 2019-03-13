1f88bfd4f9802a87648a6c92d3ececa1806539c1
Aaron <aaronferns1@yahoo.com>
2019-03-13 14:33:11 +0400

    Main project, first commit.
    Added text fade in-out effect for Title Screen.

---
+++ No Going Back Final/Assets/Scripts/TextFadeInOut.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextFadeInOut : MonoBehaviour
+{
+    Text text;
+    float rate;
+
+	void Start ()
+    {
+        text = GetComponent<Text>();
+        rate = 0.01f;
+	}
+	
+	void Update ()
+    {
+        FadeInOut();
+	}
+
+    void FadeInOut()
+    {
+        if(text.color.a >= 1 || text.color.a <= 0)
+        {
+            rate = -rate;
+        }
+        text.color += new Color(0, 0, 0, rate);
+    }
+}
